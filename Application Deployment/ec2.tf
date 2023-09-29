3bdc82fdb86fa731e0126072e2817eeea1dc4ff1
Mathesh <144098846+mathesh-me@users.noreply.github.com>
2023-09-29 15:54:53 +0530

    Update ec2.tf

--- Application Deployment/ec2.tf
+++ Application Deployment/ec2.tf
@@ -4,4 +4,28 @@ resource "aws_instance" "server" {
   key_name      = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("/c/Users/Mathesh M/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+   }
+
+   # File provisioner to copy a file from local to the remote EC2 instance
+   provisioner "file" {
+    source      = "../Flask Application/app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+   }
+
+   provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "sudo python3 app.py",
+    ]
+   }
 }
